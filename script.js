1e48dd6dd902e43001bac81a7ac276e6a55549d6
Dima <goldrarian@gmail.com>
2020-09-12 23:23:02 +0300

    9-12-20 Adaptive v2

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
 /* BURGER MENU */
 const burger = document.querySelector('.burger'),
-      nav = document.querySelector('.nav');
+      nav = document.querySelector('.nav'),
+      intro = document.querySelector('.intro').scrollHeight,
+      header = document.querySelector('.header');
 
 burger.addEventListener('click', () => {
     if (burger.classList.contains('active')) {
@@ -16,23 +18,58 @@ burger.addEventListener('click', () => {
 });
 
 
+ /* FIXED HEADER */
+
+let lastScroll = 0;
+
+window.addEventListener('scroll', function () {
+    
+    let height = pageYOffset;
+
+    if (height > intro) {
+        if (height > lastScroll) {
+            header.classList.remove('fixed');
+        } else {
+            header.classList.add('fixed');
+        }
+    } else {
+        header.classList.remove('fixed');
+    }
+
+    lastScroll = height;
+});
+
 /* SMOOTH SCROLL */
 
 const blocks = document.querySelectorAll('a[href*="#"');
 
-blocks.forEach(function(item) {
-    item.addEventListener('click', (event)=>{
-        event.preventDefault();
-        const blockId = item.getAttribute('href');
-        document.querySelector("" + blockId).scrollIntoView({
-            block: "start",
-            behavior: "smooth"    
+function scrollMain(scrollDelay) {
+    blocks.forEach(function(item) {
+        item.addEventListener('click', (event)=>{
+            event.preventDefault();
+            const blockId = item.getAttribute('href');
+            document.querySelector("" + blockId).scrollIntoView({
+                block: "start",
+                behavior: "smooth"    
+            });
+            nav.style.left = "-100%";
+            burger.classList.remove('active');
+
+            /* header.classList.remove('fixed');
+            header.style.display = 'none'; */
+            /* lastScroll = 0; */
+            scrollDelay();
         });
+        
     });
+   
+}
+
+
+
+scrollMain(function() {
+    header.classList.remove('fixed');
 });
-
-
-
 
 
 });
