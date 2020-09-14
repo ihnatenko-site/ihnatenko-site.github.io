94fb2edd471e31d1f22b11f4113a8166d491d640
Dima <goldrarian@gmail.com>
2020-09-14 22:16:50 +0300

    Adaptive 9-14-20

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const burger = document.querySelector('.burger'),
       intro = document.querySelector('.intro').scrollHeight,
       header = document.querySelector('.header');
 
+
 burger.addEventListener('click', () => {
     if (burger.classList.contains('active')) {
         burger.classList.remove('active');
@@ -27,11 +28,12 @@ window.addEventListener('scroll', function () {
     let height = pageYOffset;
 
     if (height > intro) {
-        if (height > lastScroll) {
+        header.classList.add('fixed');
+        /* if (height > lastScroll) {
             header.classList.remove('fixed');
         } else {
             header.classList.add('fixed');
-        }
+        } */
     } else {
         header.classList.remove('fixed');
     }
@@ -43,33 +45,31 @@ window.addEventListener('scroll', function () {
 
 const blocks = document.querySelectorAll('a[href*="#"');
 
-function scrollMain(scrollDelay) {
-    blocks.forEach(function(item) {
-        item.addEventListener('click', (event)=>{
-            event.preventDefault();
-            const blockId = item.getAttribute('href');
-            document.querySelector("" + blockId).scrollIntoView({
-                block: "start",
-                behavior: "smooth"    
-            });
-            nav.style.left = "-100%";
-            burger.classList.remove('active');
 
-            /* header.classList.remove('fixed');
-            header.style.display = 'none'; */
-            /* lastScroll = 0; */
-            scrollDelay();
+blocks.forEach(function(item) {
+    item.addEventListener('click', (event)=>{
+        event.preventDefault();
+        const blockId = item.getAttribute('href');
+        document.querySelector("" + blockId).scrollIntoView({
+            block: "start",
+            behavior: "smooth"    
         });
-        
+        nav.style.left = "-100%";
+        burger.classList.remove('active');
+
+        /* header.classList.remove('fixed');
+        header.style.display = 'none'; */
+        /* lastScroll = 0; */
+       
     });
-   
-}
-
-
-
-scrollMain(function() {
-    header.classList.remove('fixed');
+    
 });
+   
+
+
+
+
+
 
 
 });
